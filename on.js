cb6090a5cd62602843726c7e7d5a00e703f54f33
Vincent GRENIER <105703342+just-a-few-code-lines@users.noreply.github.com>
2022-05-17 12:29:18 +0200

    Add files via upload

--- on.js
+++ on.js
@@ -9,26 +9,44 @@ EventTarget.prototype.On = function( event, selector, func ) {
 		this._on = {};
 
 	if( this._on[event] ) {
-		// Add handler to an existing event
+		// Add to existing event
 		this._on[event].push( {selector:selector, func:func} );
-		return;
 	}
 
-	// New event. Register and setup handler
-	this._on[event] = [ {selector:selector, func:func} ];
+	else {
+		// New event.
 
-	// Setup base for querySelectorAll
-	var base = this === window ? document : this;
+		// Register
+		this._on[event] = [ {selector:selector, func:func} ];
 
-	// On this event
-	this.addEventListener( event, evt => {
-		// Iterate throught the On calls
-		this._on[event].forEach( i => {
-			// If the target is concerned
-			if( Array.prototype.includes.call(base.querySelectorAll( i.selector ), evt.target) ) {
-				// Call the listener
-				i.func.call(evt.target,evt);
+		// Setup listener
+		this.addEventListener( event, evt => {
+			let e, f;
+
+			// On receiving the event, build the list of the elements from evt.target to this
+			// They potentially have setup a listener for the event
+			// Build a parallel an array that will contain the listener functions to call
+			let elements = [];
+			let calls = []
+			for( e = evt.target; e && e != this.parentNode; e = e.parentNode ) {
+				elements.push(e);
+				calls.push([]);
+			}
+
+			// for each {selector;function} of this event
+			this._on[event].forEach( i => {
+				// use the selector to filter the concerned elements
+				for( e = evt.target.closest(i.selector); e && elements.includes(e); e = e.parentNode.closest(i.selector) )
+					// and for each one, archive the function to call
+					calls[elements.indexOf(e)].push(i.func);
+			});
+
+			// Finally, iterate once again from evt.target to this to make the event bubbling
+			for( e = 0; e < elements.length && evt.bubbles && !evt.cancelBubble; e++ ) {
+				for( f = 0; f < calls[e].length && evt.bubbles && !evt.cancelBubble; f++ ) {
+					calls[e][f].call(elements[e],evt);
+				}
 			}
 		});
-	});
+	}
 };
